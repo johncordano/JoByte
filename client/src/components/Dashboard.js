a684c95e55c79096b5c580c32fa89a0c25b43b84
liannaccone <lciannaccone@gmail.com>
2018-03-30 14:25:37 -0700

    adding ResearchingTable component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -11,21 +11,37 @@ import ChartPurple from '../images/chart-purple.svg';
 class Dashboard extends React.Component {
   state = {
     MyJobsTableVisible: false,
-    MyActionsTableVisible: false
+    MyActionsTableVisible: false,
+    ResearchingTableVisible: false
   };
 
+  hideAllTables = () => {
+    this.setState({
+    MyJobsTableVisible: false,
+    MyActionsTableVisible: false,
+    ResearchingTableVisible: false
+    })
+  }
+
   handleMyJobsClick = () => {
-    this.setState({MyActionsTableVisible: false})
+    this.hideAllTables();
     let { MyJobsTableVisible } = this.state.MyJobsTableVisible;
     MyJobsTableVisible = MyJobsTableVisible ? false : true;
     this.setState({ MyJobsTableVisible });
   };
 
   handleMyActionsClick = () => {
-    this.setState({MyJobsTableVisible: false})
+    this.hideAllTables();
     let { MyActionsTableVisible } = this.state.MyActionsTableVisible;
     MyActionsTableVisible = MyActionsTableVisible ? false : true;
     this.setState({ MyActionsTableVisible });
+  };
+
+  handleResearchingClick = () => {
+    this.hideAllTables();
+    let { ResearchingTableVisible } = this.state.ResearchingTableVisible;
+    ResearchingTableVisible = ResearchingTableVisible ? false : true;
+    this.setState({ ResearchingTableVisible });
   };
 
   render() {
@@ -34,20 +50,28 @@ class Dashboard extends React.Component {
         <Navbar />
         <div className="centralized">
           <div className="container-dashboard">
+
             <div className="total-applied" onClick={this.handleMyJobsClick.bind(this)}>
               <h3>My Jobs</h3>
-              <p id="jobCount">20</p>
+              <p id="jobCount">{this.props.jobs.length}</p>
               <img src={ChartBlue} alt="Char blue graphic" />
             </div>
 
             <div className="scheduled-interview" onClick={this.handleMyActionsClick.bind(this)}>
               <h3>To do's</h3>
-              <p>12</p>
+              <p>{this.props.actions.length}</p>
               <img src={ChartPurple} alt="Char blue graphic" />
             </div>
+
+            <div className="total-researching" onClick={this.handleResearchingClick.bind(this)}>
+              <h3>Researching</h3>
+            </div>
+     
+
           </div>
           {this.state.MyJobsTableVisible ? <Myjobs jobs={this.props.jobs} /> : null}
           {this.state.MyActionsTableVisible ? <MyActions actions={this.props.actions} /> : null}
+          {/*this.state.ResearchingTableVisible ? <ResearchingTable actions={this.props.jobs} /> : null */}
         </div>
       </div>
     );
---
+++ client/src/components/ResearchingTable.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import ViewJob from './ViewJob';
+// import SummaryGraph from './SummaryGraph';
+
+class ResearchingTable extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="table-job">
+        <h2>Your saved jobs</h2>
+        <table className="rtable">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Position</th>
+              <th>Link</th>
+              <th>Status</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody className="tbody saved-jobs" id="saved-jobs">
+            {this.props.jobs.filter(data => {
+              return (
+                <tr key={data._id}>
+                  <td>{data.company}</td>
+                  <td>{data.position}</td>
+                  <td>{data.link}</td>
+                  <td>{data.status}</td>
+                  <td>
+                    <Link
+                      to={{
+                        pathname: '/job/view',
+                        state: {
+                          jobInfo: data
+                        }
+                      }}
+                    >
+                      <button data-id={data._id} id="view-btn">
+                        View
+                      </button>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default MyTable;
