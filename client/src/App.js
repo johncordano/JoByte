c78b9e0116b304c162c3fe3be83ced2c2dab8985
liannaccone <lciannaccone@gmail.com>
2018-03-31 14:25:01 -0700

    removing API calls/functions from app.js and putting them in dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -6,45 +6,39 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import AddJob from './components/AddJob';
 import ViewJob from './components/ViewJob';
-import API from './utils/API';
+// import API from './utils/API';
 // import Myjobs from './components/Myjobs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      jobsArray: [],
-      actionsArray: []
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     jobsArray: [],
+  //     actionsArray: []
+  //   };
+  // }
 
-  componentDidMount() {
-    this.loadJob();
-    this.loadActions();
-  }
+  // componentDidMount() {
+  //   this.loadJob();
+  //   this.loadActions();
+  // }
 
-  loadJob = () => {
-    API.getJob()
-      .then(res => this.setState({ jobsArray: res.data }))
-      .catch(err => console.log(err));
-  };
+  // loadJob = () => {
+  //   API.getJob()
+  //     .then(res => this.setState({ jobsArray: res.data }))
+  //     .catch(err => console.log(err));
+  // };
 
-  loadActions = () => {
-    API.getAllAction()
-      .then(res => this.setState({ actionsArray: res.data }))
-      .catch(err => console.log(err));
-  };
+  // loadActions = () => {
+  //   API.getAllAction()
+  //     .then(res => this.setState({ actionsArray: res.data }))
+  //     .catch(err => console.log(err));
+  // };
 
   render() {
     return (
       <div>
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return <Dashboard jobs={this.state.jobsArray} actions={this.state.actionsArray}/>;
-          }}
-        />
+        <Route exact path = "/" component={Dashboard} />
         <Route exact path="/job/new" component={AddJob} />
         <Route path="/job/view" component={ViewJob} />
       </div>
