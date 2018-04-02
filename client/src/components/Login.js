51743bdfab60a7cc889519a70b14cb9018194417
paulakedouk <paulakedouk@gmail.com>
2018-04-02 15:24:57 -0700

    Adjust a typo

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import InitialImg from '../images/initial-img.jpg';
+import InitialImg from '../images/initial-img.jpeg';
 import Logo from '../images/logo.svg';
 import API from '../utils/API';
 import { withRouter } from 'react-router-dom';
@@ -7,75 +7,71 @@ import { Route, Redirect } from 'react-router';
 
 class Login extends Component {
   state = {
-    name: "",
-    email: "",
-    password: "",
-    redirect: false,
+    name: '',
+    email: '',
+    password: '',
+    redirect: false
   };
-  
+
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
   };
-  
+
   handleFormSubmitExistingAccount = event => {
     event.preventDefault();
-    console.log("login");
+    console.log('login');
     API.getAccount({
       email: this.state.email,
-      password: this.state.password,
+      password: this.state.password
     })
-    .then(res => {
-      console.log("login res", res);
+      .then(res => {
+        console.log('login res', res);
 
-      if (res.status === 200) {
-        this.setState({redirect: true});
-         }
-        else {
-          console.log("check your credentials")
+        if (res.status === 200) {
+          this.setState({ redirect: true });
+        } else {
+          console.log('check your credentials');
         }
-      }
-    )
-    .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
   };
 
   render() {
     const { redirect } = this.state;
     if (redirect) {
-      return <Redirect to={"/dashboard"} />
+      return <Redirect to={'/dashboard'} />;
     }
     return (
-      
-   <div className="login-container">
-      <img className="login-img" src={InitialImg} alt="Initial img" />
-      <div className="signin">
-        <img src={Logo} alt="Logo" />
-        <h2>Sign in to existing account</h2>
-        <form className="existing-form">
-          <input
-            className="input-label"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-            name="email"
-            placeholder="Email address (required)"
-          />
-          <input
-            className="input-label"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-            name="password"
-            placeholder="Password (required)"
-          />
-        <button className="signin-btn" onClick={this.handleFormSubmitExistingAccount}>
-            Sign in
-          </button>
-        </form>
-      </div>    
-    </div>
-    )
-
+      <div className="login-container">
+        <img className="login-img" src={InitialImg} alt="Initial img" />
+        <div className="signin">
+          <img src={Logo} alt="Logo" />
+          <h2>Sign in to existing account</h2>
+          <form className="existing-form">
+            <input
+              className="input-label"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+              name="email"
+              placeholder="Email address (required)"
+            />
+            <input
+              className="input-label"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              name="password"
+              placeholder="Password (required)"
+            />
+            <button className="signin-btn" onClick={this.handleFormSubmitExistingAccount}>
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
