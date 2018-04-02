dff293ab7d7ba1ccdef1ab9f90394e18f4bb8e68
paulakedouk <paulakedouk@gmail.com>
2018-04-02 14:16:56 -0700

    Adjust login page

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,18 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import AddJob from './components/AddJob';
 import ViewJob from './components/ViewJob';
+import Login from './components/Login';
 // import API from './utils/API';
-// import Myjobs from './components/Myjobs';
 
 class App extends Component {
-
   render() {
     return (
       <div>
-        {/*<Route exact path = "/signin" component={Signin} />*/}
-        <Route exact path = "/" component={Dashboard} />
+        {/* <Route exact path = "/signin" component={Signin} /> */}
+
+        <Route exact path="/login" component={Login} />
+
+        <Route exact path="/" component={Dashboard} />
         <Route exact path="/job/new" component={AddJob} />
         <Route path="/job/view" component={ViewJob} />
       </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+import InitialImg from '../images/initial-img.jpg';
+import Logo from '../images/logo.svg';
 
 class Login extends Component {
-  state = {
-    user: []
-  };
-
-  componentDidMount() {
-    setState = {};
-  }
-
   render() {
-    return <div className="App" />;
+    return (
+      <div className="login-container">
+        <img className="login-img" src={InitialImg} alt="Initial img" />
+        <div className="signin">
+          <img src={Logo} alt="Logo" />
+          <div className="description">
+            <p>The platform to organize and connect your job search.</p>
+          </div>
+
+          <div className="buttons">
+            <button className="login-btn enter-btn">Login</button>
+
+            <div className="separate">
+              <div className="line-left" />
+              or
+              <div className="line-right" />
+            </div>
+
+            <button className="signup-btn">Sign Up</button>
+            <button id="google-btn">Sign In With Google</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
